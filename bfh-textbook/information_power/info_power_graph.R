eca4af6e88cd0353363924cf9b87d2f9dc27e9c1
Simon Halliday <shalliday@smith.edu>
2015-08-05 16:24:02 -0400

    Initial commit of info power graph

---
+++ bfh-textbook/information_power/info_power_graph.R
@@ -0,0 +1,86 @@
+require(ggplot2)
+pdf(file = "brf.pdf", width = 9, height = 6)
+brfFn <- function(p, delta = 5) {
+  1 - (2 * delta) /p
+}
+
+isovhigh <- function(p, delta = 5, v = 20){
+  (sqrt(p^2 - 4 * delta * v) - p + 2*v)/( 2 * v )
+}
+
+isovlow <- function(p, delta = 5, v = 20){
+  (-sqrt(p^2 - 4 * delta * v) - p + 2*v)/( 2 * v )
+}
+
+solowCondition <- function(p, delta = 5){
+  (p*(1/(8*delta)))
+}
+
+COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
+par(mar =  c(5, 5, 4, 2))
+xlims <- c(0, 40)
+ylims <- c(0, 1)
+
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = expression(paste("Price, ", p)),
+     ylab = expression(paste("Quality, ", q)),
+     xaxt = "n", yaxt = "n", cex.lab = 2, bty = "n")
+
+npts <- 500 
+npts2 <- 501
+xx1 <- seq(xlims[1], xlims[2], length.out = npts)
+xx2 <- seq(xlims[2], length.out = npts)
+xx3 <- seq(xlims[1], xlims[2], length.out = npts2)
+lines(xx1, brfFn(xx1), col = COL[2], lwd = 4)
+lines(xx3, isovhigh(xx3, delta = 5), col = COL[1], lwd = 4)
+lines(xx3, isovlow(xx3, delta = 5), col = COL[1], lwd = 4)
+lines(xx1, solowCondition(xx1, delta = 5), col = COL[3], lwd = 4)
+
+ticksy <- c(0, 0.5, 1)
+
+axis(1, at = xlims, pos = 0)
+axis(2, at = ylims, pos = 0, las=1)
+
+# add_legend <- function(...) {
+#   opar <- par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), 
+#               mar=c(0, 0, 0, 0), new=TRUE)
+#   on.exit(par(opar))
+#   plot(0, 0, type='n', bty='n', xaxt='n', yaxt='n')
+#   legend(...)
+# }
+
+#Annotation of the three graphs and the NE
+text(5, 0.3, expression(paste("Principal's BR ", frac(q, p) ," = ", frac(1, 4)*delta)))
+text(35, 0.62, expression(paste("BRF q = ", 1 - frac(2*delta, p))))
+text(28, 0.1, expression(paste("Iso-v curve v = ", v,"*")))
+text(19, 0.55, expression(paste("NE")))
+
+#Annotation of the important points on the axes
+mtext(expression(paste(2*delta)), side = 1, at = 10)
+mtext(expression(paste(4*delta)), side = 1, at = 20)
+mtext(expression(paste(frac(1,2))), las = 1, side = 2, at = 0.5)
+
+# ticksx <- c(0, 10, 20, 40)
+# axis(1, at = ticksx)
+# ticksy <- c(0, 0.5, 1)
+# axis(2, at = ticksy)
+
+#Lines for the coordinates of the Nash equilbrium
+segments(20, 0, 20, 0.5, lty = 2, col = "darkgray")
+segments(0, 0.5, 20, 0.5, lty = 2, col = "darkgray")
+
+#Add a point for the NE
+points(20, 0.5, pch = 16, col = "black", cex = 1.5)
+
+# add_legend("topright", legend=c(
+#   expression(paste("Agent's Indifference Curve: v = v*", )),
+#   expression(paste("Agent's Best Response: q = 1 - (2*delta/p)",)), 
+#   expression(paste("Solow Condition: q = p*(delta/2)",))
+# ),
+# col = COL, lty = 1, lwd = 2, cex=1, bty = "n")
+
+
+
+dev.off()
+
