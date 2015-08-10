795e8fdc54782fc507736038913612a1151d6c06
Simon Halliday <shalliday@smith.edu>
2015-08-10 08:38:36 -0400

    Commit of new figures for Chapter 11 after chat with Sam. Arrows have triangular heads and only point vertically or horizontally. Labels are more fully specified. Fig 4 included.

--- bfh-textbook/information_power/ch11_fig2.R
+++ bfh-textbook/information_power/ch11_fig2.R
@@ -1,10 +1,14 @@
 require(ggplot2)
 require(shape)
+require(plotrix)
 pdf(file = "ch11_fig2.pdf", width = 9, height = 7)
+
+#Set parameters for graphics
+axislabelsize <- 1.5
+
 disutilityFn <- function(q, delta = 5) {
   delta /(1 - q)
 }
-
 
 #COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99")
@@ -16,7 +20,7 @@ ylims <- c(0, 40)
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("Quality, ", q)),
      ylab = expression(paste("Disutility of providing quality, ", u)),
-     xaxt = "n", yaxt = "n", cex.lab = 2, bty = "n")
+     xaxt = "n", yaxt = "n", cex.lab = axislabelsize, bty = "n")
 
 npts <- 500 
 npts2 <- 501
@@ -33,7 +37,7 @@ lines(xx1, disutilityFn(xx1), col = COL[1], lwd = 4)
 
 #Customize ticks and labels for the plot
 ticksy <- c(0, 5, 40)
-ylabels <- c(0, expression(paste(delta)), 40)
+ylabels <- c(0, 5, 40)
 ticksx <- c(0, 1, 1.1)
 xlabels <- c(0, 1.0, NA)
 axis(1, at = ticksx, pos = 0, labels = xlabels)
@@ -46,8 +50,17 @@ text(0.7, 35, expression(paste("Distutility = ", frac(delta, (1 - q)))))
 segments(1, 0, 1, 42, lty = 2, lwd = 3, col = "darkgray")
 
 #Arrow to Slope of BRF
-Arrows(0.35, 11.5, 0.375, 9.5, col = "black", lty = 1, lwd = 3)
+Arrows(0.5, 13, 0.58, 13, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 text(0.35, 13, expression(paste("Slope = " - u[q], " = ", frac(delta, (1 - q)^2))))
+
+#Text to indicate delta = 5
+text(0.2, 38, expression(paste("Disutility of effort")))
+text(0.2, 36, expression(paste("set to ", delta, " = 5")))
+
+#Text for max quality
+text(1.08, 38, expression(paste("Maximum")))
+text(1.08, 36, expression(paste("level of")))
+text(1.08, 34, expression(paste("quality, q")))
 
 
 dev.off()
--- bfh-textbook/information_power/ch11_fig5.R
+++ bfh-textbook/information_power/ch11_fig5.R
@@ -1,20 +1,16 @@
 require(ggplot2)
 require(shape)
-pdf(file = "ch11_fig1.pdf", width = 9, height = 7)
+pdf(file = "ch11_fig5.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
 axislabelsize <- 1.5
 
 brfFn <- function(p, delta = 5) {
-  1 - (2*delta) /p
+  1 - (2 * delta) /p
 }
 
 solowCondition <- function(p, delta = 5){
   (p*(1/(8*delta)))
-}
-
-solowInfeas <- function(p, delta = 5){
-  (p*(1/(4*delta)))
 }
 
 #COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
@@ -34,42 +30,49 @@ npts2 <- 501
 #Specify the sequences of points for graphing. 
 xx1 <- seq(10, xlims[2], length.out = npts)
 xx2 <- seq(xlims[1], xlims[2], length.out = npts)
-xx3 <- seq(xlims[1], xlims[2], length.out = npts2)
-xx4 <- seq(xlims[1], 10, length.out = npts2)
 
 #Draw the lines for the graphs
 lines(xx1, brfFn(xx1), col = COL[2], lwd = 4)
 lines(xx2, solowCondition(xx2, delta = 5), col = COL[3], lwd = 4)
-lines(xx2, solowInfeas(xx2, delta = 5), col = COL[1], lwd = 4, lty = 2)
 
 #Customize ticks and labels for the plot
 ticksy <- c(0, 0.5, 1)
 ylabels <- c(0, expression(paste(frac(1,2))), 1)
 ticksx <- c(0, 10, 20, 40)
-xlabels <- c(0, NA, NA, 40)
+xlabels <- c(0, expression(paste(2*delta)), expression(paste(4*delta)), 40)
 axis(1, at = ticksx, pos = 0, labels = xlabels)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
 
 #Annotation of the three graphs and the NE
-text(35, 0.98, expression(paste("Iso-profit")))
-text(15, 0.98, expression(paste("Infeasible Iso-profit")))
-text(35, 0.63, expression(paste("Best response function")))
+text(33, 0.98, expression(paste("Iso-profit: ", q," = ", frac(p, 8*delta))))
+text(35, 0.64, expression(paste("Best Response")))
+text(35, 0.57, expression(paste("Function = q = ", 1 - frac(2*delta, p))))
 text(16, 0.52, expression(paste("Nash Equilibrium")))
 
 #Lines for the coordinates of the Nash equilbrium
 segments(20, 0, 20, 0.5, lty = 2, col = "darkgray")
 segments(0, 0.5, 20, 0.5, lty = 2, col = "darkgray")
 
+#Iso-profit slope annotation
+text(25, 0.81, expression(paste("Slope = ", frac(1, 8*delta))))
+Arrows(25, 0.79, 25, 0.66, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+
+#BRF Slope annotation
+text(25, 0.41, expression(paste("Slope = ", frac(2*delta, p^2))))
+Arrows(25, 0.43, 25, 0.56, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+
 #Add a point for the NE
 points(20, 0.5, pch = 16, col = "black", cex = 1.5)
 
-#Arrow to Slope of BRF
-Arrows(14.2, 0.12, 12.2, 0.12, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
-text(16.8, 0.12, expression(paste("Slope = ", q[p])))
+#Add a ray and a point for d.  
+segments(0, 0, 20, 1, lty = 2, lwd = 3, col = "darkgray")
+points(10, 0.5, pch = 16, col = "black", cex = 1.2)
+text(9.6, 0.53, expression(paste("d")))
 
-#Arrow to Slope of isoprofit
-Arrows(13, 0.80, 15, 0.80, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
-text(10.2, 0.80, expression(paste("Slope = ", frac(q, p))))
+#Add a ray and a point for a. 
+segments(0, 0, 20, 0.25, lty = 2, lwd = 3, col = "darkgray")
+points(11.8, 0.15, pch = 16, col = "black", cex = 1.2)
+text(11.3, 0.17, expression(paste("c")))
 
 dev.off()
 
